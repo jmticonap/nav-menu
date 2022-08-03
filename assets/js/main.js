e0ba27552811bb106282a1feacf21f76a1048668
juancho <jm.ticona.p@gmail.com>
2022-08-02 22:26:34 -0500

    [FIX] toggle event is JS controlet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 
 const btn_toggle__input = document.querySelector("#btn_toggle")
 const btn_toggle__lbl = document.querySelector(".btn_toggle__lbl")
+const btn_toggle__ul = document.querySelector(".menu-links")
 const btn_toggle__li__a = document.querySelectorAll(".menu-links a")
 const img__background = document.querySelector(".img-background")
 const page_cover = document.querySelector(".page-cover")
@@ -8,28 +9,31 @@ const title_h1 = document.querySelector(".page-cover__title__h1")
 
 
 function click_toggle() {
-    //console.log( btn_toggle__input.checked )
+    if (btn_toggle__input.checked) {
+        btn_toggle__ul.style = `display: none`
+    } else {
+        btn_toggle__ul.style = `display: flex`
+    }
 }
 
 function toggle_off() {
-    if (!btn_toggle__input.checked) btn_toggle__input.checked = true
+    btn_toggle__input.checked = true
+    btn_toggle__ul.style = `display: none`
 }
 
 function title_h1_resize() {
     let font_size = title_h1.style.fontSize
-  
-    if ( font_size === "" || parseFloat(font_size.substring(0, font_size.length - 3)) <= 7.0) {
+
+    if (font_size === "" || parseFloat(font_size.substring(0, font_size.length - 3)) <= 7.0) {
         const factor = 0.008
         let size = document.body.clientWidth * factor
-        size = size>7?7:size
+        size = size > 7 ? 7 : size
         title_h1.style = `font-size: ${size}rem`
     }
-
 }
 
-
 document.body.onload = function () {
-    btn_toggle__lbl.addEventListener("click", click_toggle, false)
+    btn_toggle__input.addEventListener("change", click_toggle, false)
     document.body.onresize = function () {
         toggle_off()
         title_h1_resize()
@@ -37,5 +41,4 @@ document.body.onload = function () {
     btn_toggle__li__a.forEach((itm) => {
         itm.addEventListener("click", toggle_off, false)
     })
-
 }
